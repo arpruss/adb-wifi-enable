31ae03496a02ca1190a93a027e159a19b6deb7c7
Alexander Pruss <arpruss@gmail.com>
2025-11-22 23:03:31 -0600

    foo

--- app/src/main/java/mobi/omegacentauri/gotosettings_adb/gotosettings_adb.java
+++ app/src/main/java/mobi/omegacentauri/gotosettings_adb/gotosettings_adb.java
@@ -1,16 +1,22 @@
 package mobi.omegacentauri.gotosettings_adb;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.text.Layout;
@@ -19,14 +25,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -44,6 +57,7 @@ public class gotosettings_adb extends Activity {
     int port = -1;
     int pairPort = -1;
     JmDNS jmdns = null;
+    CharSequence outputData = "";
 
     private static final String TAG = "gotosettings_adb_main";
     private String adbPath;
@@ -60,6 +74,9 @@ public class gotosettings_adb extends Activity {
     private TextView pinField;
     private TextView pairPortField;
     private LinearLayout pairControls;
+    private TextView output;
+    private ScrollView outputScroller;
+    private LinearLayout scriptsView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -69,6 +86,7 @@ public class gotosettings_adb extends Activity {
 
         setContentView(R.layout.main);
 
+        scriptsView = (LinearLayout)findViewById(R.id.scripts);
         enableWiFiADBButton = findViewById(R.id.enable_wifi_adb_button);
         grantText = findViewById(R.id.grant);
         adbText = findViewById(R.id.adb);
@@ -77,12 +95,28 @@ public class gotosettings_adb extends Activity {
         pinField = (TextView) findViewById(R.id.pin);
         pairPortField = (TextView)findViewById(R.id.pair_port);
         pairControls = (LinearLayout) findViewById(R.id.pair_controls);
+        output = (TextView)findViewById(R.id.output);
+        outputScroller = (ScrollView)findViewById(R.id.output_scroller);
     }
 
     void checkPermissions() {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+                    try {
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                        Uri uri = Uri.fromParts("package", gotosettings_adb.this.getPackageName(), null);
+                        intent.setData(uri);
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                        startActivity(intent);
+                    }
+                } //TODO:lower version
+
                 if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M
                         &&
                         PackageManager.PERMISSION_DENIED ==
@@ -100,6 +134,51 @@ public class gotosettings_adb extends Activity {
         });
     }
 
+    @SuppressLint("NewApi")
+    void addButtons() {
+        Button b;
+        try {
+            File storage = new File(Environment.getExternalStorageDirectory() + "/adbscripts");
+            outputData += storage.getAbsolutePath()+" "+storage.isDirectory()+"\n";
+            File[] files = storage.listFiles();
+            Arrays.sort(files);
+            for (File f : files) {
+                b = new Button(this);
+                b.setText(f.getName());
+                b.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        runScript(f);
+                    }
+                });
+                scriptsView.addView(b);
+            }
+        }
+        catch (Exception e) {
+            Log.v(TAG, ""+e);
+        }
+        //Log.v(TAG, String.valueOf(Environment.getExternalStorageDirectory()));
+    }
+
+    private void runScript(File f) {
+        List<String> cmds = new ArrayList<>();
+        cmds.add("adb connect "+address.getHostName() + ":" + port);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            while(true) {
+                String line = br.readLine();
+                if (line == null)
+                    break;
+                if (line.startsWith("adb ")) {
+                    line = "adb -s "+address.getHostName()+":"+port+" "+line.substring(4);
+                }
+                cmds.add(line);
+            }
+            adbRun(cmds.toArray(new String[0]));
+        } catch (IOException e) {
+        }
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -107,6 +186,9 @@ public class gotosettings_adb extends Activity {
         checkPermissions();
         updateAddressPort();
         listen();
+        outputData = "";
+        scriptsView.removeAllViews();
+        addButtons();
     }
 
     @Override
@@ -145,12 +227,26 @@ public class gotosettings_adb extends Activity {
         }
     }
 
+    public boolean isPackageInstalled(String s) {
+        PackageManager pm = getPackageManager();
+        for (PackageInfo pi : pm.getInstalledPackages(0)) {
+            if (pi.packageName.equals(s))
+                return true;
+        }
+        return false;
+    }
+
     public void goToSettings(View view) {
         Intent i = new Intent();
-        i.setComponent(new ComponentName("com.android.settings", "com.android.settings.Settings"));
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+        if (isPackageInstalled("com.android.settings"))
+            i.setComponent(new ComponentName("com.android.settings", "com.android.settings.Settings"));
+        else
+            i.setComponent(new ComponentName("com.oculus.panelapp.settings", "com.oculus.panelapp.settings.SettingsActivity"));
+
+        //        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+        if (Build.VERSION.SDK_INT >=24) i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT);
         startActivity(i);
-        finish();
+        //finish();
     }
 
     public void closeListen() {
@@ -284,10 +380,16 @@ public class gotosettings_adb extends Activity {
                     pairPortField.setText(""+pairPort);
                 else
                     pairPortField.setText("");
-                if (port >= 0)
-                    adbText.setText(address.getHostAddress()+":"+port);
-                else
+                if (port >= 0) {
+                    adbText.setText(address.getHostAddress() + ":" + port);
+                    scriptsView.setVisibility(View.VISIBLE);
+                    enableWiFiADBButton.setVisibility(View.INVISIBLE);
+                }
+                else {
                     adbText.setText("");
+                    scriptsView.setVisibility(View.GONE);
+                    enableWiFiADBButton.setVisibility(View.VISIBLE);
+                }
                 if (pairPort >= 0) {
                     pinField.setVisibility(View.VISIBLE);
                     pairControls.setVisibility(View.VISIBLE);
@@ -303,10 +405,8 @@ public class gotosettings_adb extends Activity {
 
     public void noIPDMessage(View view) {
         if (port >= 0) {
-            String cmd1[] = new String[] { adbPath, "connect", ""+address.getHostName() + ":" + port,
-                        };
-            String cmd2[] = new String[] { adbPath, "-s", ""+address.getHostName() + ":" + port,
-                    "shell", "setprop", "debug.oculus.noIpdNotifier", "1" };
+            String cmd1 = "adb connect "+address.getHostName() + ":" + port;
+            String cmd2 = "adb shell setprop debug.oculus.noIpdNotifier 1";
             new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -316,15 +416,19 @@ public class gotosettings_adb extends Activity {
         }
     }
 
-    private void adbrun(String[]... cmds) {
-        for (String[] cmd : cmds) {
-            if (cmd == null)
+    private void adbRun(String[] cmds) {
+        for (String cmd : cmds) {
+            if (cmd == null || cmd.length() == 0)
                 continue;
-            String s = "";
-            for (String c : cmd)
-                s += c + " ";
-            Log.v(TAG,s);
-            ProcessBuilder builder = new ProcessBuilder(cmd);
+            cmd = cmd.trim();
+            if (! cmd.startsWith("adb ") && ! cmd.startsWith("ADB "))
+                continue;
+            Log.v(TAG,cmd);
+            outputData += ">" + cmd + "\n";
+            String[] splitAndFixed = new String[] {
+                "sh", "-c", adbPath + cmd.substring(3)
+            };
+            ProcessBuilder builder = new ProcessBuilder(splitAndFixed);
             builder.environment().put("HOME", "/data/data/mobi.omegacentauri.gotosettings_adb");
             builder.environment().put("TMPDIR", "/data/data/mobi.omegacentauri.gotosettings_adb");
             builder.redirectErrorStream(true);
@@ -343,20 +447,37 @@ public class gotosettings_adb extends Activity {
                 } catch (IOException e) {
                     return;
                 }
-                if (line != null)
+                if (line != null) {
                     Log.v(TAG, line);
+                    outputData += line + "\n";
+                }
                 else
                     break;
             }
         }
+        scrollOutput();
     }
 
-    public void devSettings(View view) {
+    private void adbrun(String... cmds) {
+        adbRun(cmds);
+    }
+
+    private void scrollOutput() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                output.setText(outputData);
+                outputScroller.smoothScrollTo(0,output.getBottom());// outputScroller.getHeight());
+            }
+        });
+    }
+
+/*    public void devSettings(View view) {
         Intent i = new Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (Build.VERSION.SDK_INT >=24) i.addFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT);
         startActivity(i);
-    }
+    } */
 
     public boolean wifi() {
         if (!wifiManager.isWifiEnabled())
@@ -366,15 +487,18 @@ public class gotosettings_adb extends Activity {
     }
 
     public void pair(View view) {
-        String cmd1[] = new String[] { adbPath, "kill-server" };
-        String cmd2[] = new String[] { adbPath, "pair", ""+address.getHostName()+":"+pairPortField.getText(), String.valueOf(pinField.getText())};
-        String cmd3[] = port < 0 ? null : new String[]{adbPath, "connect", "" + address.getHostName() + ":" + port};
-        String cmd4[] = port < 0 ? null : new String[]{adbPath, "shell", "pm", "grant", "mobi.omegacentauri.gotosettings_adb", "android.permission.WRITE_SECURE_SETTINGS"};
+        String cmd1 = "adb kill-server";
+        String cmd2 = "adb pair "+address.getHostName()+":"+pairPortField.getText()+" "+String.valueOf(pinField.getText());
+        String cmd3 = "adb connect " + address.getHostName() + ":" + port;
+        String cmd4 = "adb shell pm grant mobi.omegacentauri.gotosettings_adb android.permission.WRITE_SECURE_SETTINGS";
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                adbrun(cmd1,cmd2,cmd3,cmd4);
+                if (port < 0)
+                    adbrun(cmd1,cmd2);
+                else
+                    adbrun(cmd1,cmd2,cmd3,cmd4);
                 checkPermissions();
                 pairPort = -1;
                 updateAddressPort();
