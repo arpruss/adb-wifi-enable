ef3e325d306c89c752d1d5b3844b23410044f72f
Alexander Pruss <arpruss@gmail.com>
2025-11-24 08:54:11 -0600

    scripts

--- app/src/main/java/mobi/omegacentauri/gotosettings_adb/gotosettings_adb.java
+++ app/src/main/java/mobi/omegacentauri/gotosettings_adb/gotosettings_adb.java
@@ -52,6 +52,7 @@ public class gotosettings_adb extends Activity {
     static final String SECURE_CONNECT = "_adb_secure_connect._tcp.local.";
     static final String TLS_PAIR = "_adb-tls-pairing._tcp.local.";
     static final String SECURE_PAIR = "_adb-tls-pairing._tcp.local.";
+    static final String ADB_GRANT = "adb shell pm grant mobi.omegacentauri.gotosettings_adb android.permission.WRITE_SECURE_SETTINGS";
     static final int BUTTONS_PER_LINE = 4;
 
     InetAddress address = null;
@@ -68,6 +69,10 @@ public class gotosettings_adb extends Activity {
     private WifiManager wifiManager;
     private WifiManager.MulticastLock lock = null;
     private boolean listening = false;
+    private int connectMode = 0;
+    private static final int CONNECT_FAILED = -1;
+    private static final int CONNECT_UNKNOWN = 0;
+    private static final int CONNECT_SUCCESS = 1;
     private ServiceListener serviceListener;
     private Thread listeningThread = null;
     private TextView adbText;
@@ -90,7 +95,7 @@ public class gotosettings_adb extends Activity {
 
         scriptsView = (LinearLayout)findViewById(R.id.scripts);
         enableWiFiADBButton = findViewById(R.id.enable_wifi_adb_button);
-        grantText = findViewById(R.id.grant);
+        //grantText = findViewById(R.id.grant);
         adbText = findViewById(R.id.adb);
         adbPath = getApplicationInfo().nativeLibraryDir + "/libadb.so";
         pairButton = (Button) findViewById(R.id.pair);
@@ -118,20 +123,6 @@ public class gotosettings_adb extends Activity {
                         startActivity(intent);
                     }
                 } //TODO:lower version
-
-                if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M
-                        &&
-                        PackageManager.PERMISSION_DENIED ==
-                                checkSelfPermission("android.permission.WRITE_SECURE_SETTINGS"))
-                {
-                    enableWiFiADBButton.setEnabled(false);
-                    grantText.setVisibility(View.VISIBLE);
-                }
-                else
-                {
-                    enableWiFiADBButton.setEnabled(true);
-                    grantText.setVisibility(View.GONE);
-                }
             }
         });
     }
@@ -141,13 +132,15 @@ public class gotosettings_adb extends Activity {
         Button b;
         try {
             File storage = new File(Environment.getExternalStorageDirectory() + "/adbscripts");
-            outputData += storage.getAbsolutePath()+" "+storage.isDirectory()+"\n";
+            outputData += "Reading: "+storage.getAbsolutePath()+"\n";
+            scrollOutput();
             File[] files = storage.listFiles();
             Arrays.sort(files);
             int n = 0;
             LinearLayout line = null;
             for (File f : files) {
                 b = new Button(this);
+                b.setAllCaps(false);
                 b.setText(f.getName());
                 b.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -191,7 +184,7 @@ public class gotosettings_adb extends Activity {
                 }
                 cmds.add(line);
             }
-            adbRun(cmds.toArray(new String[0]));
+            adbrun_array(cmds.toArray(new String[0]));
         } catch (IOException e) {
         }
     }
@@ -199,6 +192,8 @@ public class gotosettings_adb extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        connectMode = CONNECT_UNKNOWN;
 
         checkPermissions();
         enableWiFiADB(false);
@@ -216,26 +211,51 @@ public class gotosettings_adb extends Activity {
     }
 
     public void enableWiFiADB(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && PackageManager.PERMISSION_DENIED ==
+                checkSelfPermission("android.permission.WRITE_SECURE_SETTINGS") &&
+                port >= 0) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    adbrun("adb kill-server",
+                            "adb connect "+getName(port),
+                            ADB_GRANT
+                            );
+                    enableWiFiADB(true);
+                }
+            }).start();
+        }
+
         enableWiFiADB(true);
     }
 
+    public boolean isWriteSecureGranted() {
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M
+                &&
+                PackageManager.PERMISSION_DENIED ==
+                        checkSelfPermission("android.permission.WRITE_SECURE_SETTINGS"))
+            return false;
+        return true;
+    }
+
+    public boolean isADBWiFiEnabled() {
+        return (1==Settings.Global.getInt(getContentResolver(), "adb_wifi_enabled", 0));
+    }
+
     public void enableWiFiADB(boolean force) {
-        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.M
-                && PackageManager.PERMISSION_DENIED ==
-                checkSelfPermission("android.permission.WRITE_SECURE_SETTINGS"))
+        if (! isWriteSecureGranted() )
             return;
 
-        if (! force) {
-            int enabled = Settings.Global.getInt(getContentResolver(), "adb_wifi_enabled", 0);
-            if (enabled == 1)
-                return;
+        if (! force && isADBWiFiEnabled()) {
+            return;
         }
 
         Log.v(TAG, "must enable");
         if (! wifi() ) {
             Log.v(TAG, "enabling wifi");
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                Toast.makeText(this, "Please activate WiFi first", Toast.LENGTH_LONG).show();;
+                Toast.makeText(this, "Please activate WiFi first", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY));
                 return;
             }
@@ -426,17 +446,38 @@ public class gotosettings_adb extends Activity {
                 else
                     pairPortField.setText("");
                 if (port >= 0) {
-                    adbText.setText(address.getHostName()+":"+port);
-                    scriptsView.setVisibility(View.VISIBLE);
-                    enableWiFiADBButton.setVisibility(View.INVISIBLE);
+                    String s = address.getHostAddress()+":"+port;
+                    if (connectMode == CONNECT_FAILED) {
+                        s += ". Connection failed. Try pairing in the system developer wireless debugging settings.";
+                        scriptsView.setVisibility(View.VISIBLE);
+                        enableWiFiADBButton.setVisibility(View.VISIBLE);
+                        enableWiFiADBButton.setEnabled(true);
+                    }
+                    else {
+                        if (connectMode == CONNECT_SUCCESS)
+                            s += ". Connection succeeded.";
+                        scriptsView.setVisibility(View.VISIBLE);
+                        enableWiFiADBButton.setVisibility(View.INVISIBLE);
+                    }
+                    adbText.setText(s);
                 }
                 else {
-                    adbText.setText("");
                     scriptsView.setVisibility(View.GONE);
                     enableWiFiADBButton.setVisibility(View.VISIBLE);
+                    if (isWriteSecureGranted()) {
+                        adbText.setText("");
+                        enableWiFiADBButton.setEnabled(true);
+                    }
+                    else {
+                        adbText.setText("Please enable/pair WiFi ADB manually.");
+                        enableWiFiADBButton.setEnabled(false);
+                    }
                 }
                 if (pairPort >= 0) {
-                    pinField.setVisibility(View.VISIBLE);
+                    if (pinField.getVisibility() != View.VISIBLE) {
+                        pinField.setVisibility(View.VISIBLE);
+                        pinField.setText("");
+                    }
                     pairControls.setVisibility(View.VISIBLE);
                 }
                 else {
@@ -461,7 +502,7 @@ public class gotosettings_adb extends Activity {
         }
     }
 
-    private void adbRun(String[] cmds) {
+    private void adbrun_array(String[] cmds) {
         for (String cmd : cmds) {
             if (cmd == null || cmd.length() == 0)
                 continue;
@@ -470,6 +511,11 @@ public class gotosettings_adb extends Activity {
                 continue;
             Log.v(TAG,cmd);
             outputData += ">" + cmd + "\n";
+            boolean connecting = cmd.substring(4).startsWith("connect ");
+            if (connecting) {
+                Log.v(TAG, "connecting");
+                connectMode = CONNECT_UNKNOWN;
+            }
             String[] splitAndFixed = new String[] {
                 "sh", "-c", adbPath + cmd.substring(3)
             };
@@ -483,28 +529,38 @@ public class gotosettings_adb extends Activity {
             } catch (IOException e) {
                 return;
             }
-            //InputStream error = Runtime.getRuntime().exec(cmd/*,env*/).getErrorStream();
             BufferedReader r = new BufferedReader(new InputStreamReader(output));
             while (true) {
                 String line = null;
                 try {
                     line = r.readLine();
+                    if (connecting && line != null) {
+                        if (line.startsWith("connected to") || line.startsWith("already connected")) {
+                            Log.v(TAG, "success");
+                            connectMode = CONNECT_SUCCESS;
+                        }
+                        else if (line.toLowerCase().contains("cannot connect") ||
+                            line.toLowerCase().contains("failed"))
+                            connectMode = CONNECT_FAILED;
+                    }
                 } catch (IOException e) {
                     return;
                 }
                 if (line != null) {
-                    Log.v(TAG, line);
                     outputData += line + "\n";
                 }
                 else
                     break;
             }
+            if (connecting && connectMode == CONNECT_FAILED)
+                break;
         }
         scrollOutput();
+        updateAddressPort();
     }
 
     private void adbrun(String... cmds) {
-        adbRun(cmds);
+        adbrun((String[])cmds);
     }
 
     private void scrollOutput() {
@@ -512,7 +568,13 @@ public class gotosettings_adb extends Activity {
             @Override
             public void run() {
                 output.setText(outputData);
-                outputScroller.smoothScrollTo(0,output.getBottom());// outputScroller.getHeight());
+                outputScroller.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        outputScroller.smoothScrollTo(0, output.getBottom());
+                    }
+                });
+                // outputScroller.getHeight());
             }
         });
     }
@@ -533,9 +595,9 @@ public class gotosettings_adb extends Activity {
 
     public void pair(View view) {
         String cmd1 = "adb kill-server";
-        String cmd2 = "adb pair "+address.getHostName()+":"+pairPortField.getText()+" "+String.valueOf(pinField.getText());
+        String cmd2 = "adb pair "+"127.0.0.1"/*address.getHostName()*/+":"+pairPortField.getText()+" "+String.valueOf(pinField.getText());
         String cmd3 = "adb connect " + getName(port);
-        String cmd4 = "adb shell pm grant mobi.omegacentauri.gotosettings_adb android.permission.WRITE_SECURE_SETTINGS";
+        String cmd4 = ADB_GRANT;
 
         new Thread(new Runnable() {
             @Override
@@ -544,7 +606,7 @@ public class gotosettings_adb extends Activity {
                     adbrun(cmd1,cmd2);
                 else
                     adbrun(cmd1,cmd2,cmd3,cmd4);
-                checkPermissions();
+//                checkPermissions();
                 pairPort = -1;
                 updateAddressPort();
             }
